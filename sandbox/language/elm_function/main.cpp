8e896501bf8febd7bfc9cf09bc7e62f9410d299b
athile <athile.g@gmail.com>
2010-08-24 18:58:26 +0100

    Update

--- sandbox/language/elm_function/main.cpp
+++ sandbox/language/elm_function/main.cpp
@@ -3,6 +3,40 @@
 #include <string>
 #include <map>
 
+
+std::string
+file_to_string  (const char* filename)
+{
+    FILE* fp = fopen(filename, "r");
+    if (fp)
+    {
+        std::string out;
+
+        // Check the file size
+        fseek(fp, 0, SEEK_END);
+        const size_t bufferSize = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+
+        // Allocate the buffer
+        out.resize(bufferSize, '\0');
+
+        // Read in the file
+        fread(&out[0], sizeof(char), bufferSize, fp);
+        fclose(fp);
+
+        // CRLF issues may mean the file size is larger than the string size.
+        // Correct that here.
+        size_t end = out.find('\0');
+        if (end != std::string::npos)
+            out.resize(end);
+
+        return out;
+    }
+    else
+        throw "File not found";
+}
+
+
 /* 
     Compute using recursion rather than Binet's formula in order to test
     out the virtual machine.
@@ -19,6 +53,8 @@ int
 main (int argc, char** argv)
 {
     const int kMax = 30;
+
+    std::string script = file_to_string("data/fibonacci_basic.elm");
 
     std::cout << "Computing Fibonacci Numbers" << std::endl;
     for (int i = 0; i <= kMax; i+= 5)
