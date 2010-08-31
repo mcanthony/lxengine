ca352de8c88c22f123522bdce06a66614eb79832
athile <athile.g@gmail.com>
2010-08-31 10:56:22 +0100

    Add minimal OGRE sample

--- lx0/build/gen_vs10.bat
+++ lx0/build/gen_vs10.bat
@@ -1,5 +1,13 @@
 @echo off
-call "%VS100COMNTOOLS%vsvars32.bat"
+
+@rem vsvars32.bat does not check if it has already been run and can overflow
+@rem the PATH variable if it is run continually
+@rem ----
+@if "%GEN_VS10_RUN%" == "" (
+   call "%VS100COMNTOOLS%vsvars32.bat"
+   set "GEN_VS10_RUN=1"
+)
+
 echo.
 cmake ..\dev -G "Visual Studio 10"
 echo.
@@ -7,4 +15,6 @@ dir /b *.sln
 echo.
 echo To build from the command line, run "msbuild lx0.sln".
 echo Otherwise, run "devenv lx0.sln" to bring up the IDE.
+echo Run "msbuild install.vcxproj" once to install dependent 
+echo binaries.
 echo.
---
+++ lx0/dev/samples/ogre/minimal/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <Ogre/Ogre.h>
+
+class LxWindowEventListener : public Ogre::WindowEventListener
+{
+public:
+    virtual void windowClosed(Ogre::RenderWindow* pRenderWindow)
+    {
+         Ogre::Root::getSingletonPtr()->queueEndRendering();
+    }
+};
+
+int main (int argc, char** argv)
+{
+    try
+    {
+        // Use default arguments to Root
+        std::auto_ptr<Ogre::Root> spRoot( new Ogre::Root );   
+
+        if (spRoot->showConfigDialog())
+        {
+            std::auto_ptr<LxWindowEventListener> spWindowEventListener(new LxWindowEventListener);
+
+            Ogre::RenderWindow* pRenderWindow = spRoot->initialise(true, "OGRE Example" );
+            Ogre::WindowEventUtilities::addWindowEventListener(pRenderWindow, spWindowEventListener.get());
+
+            spRoot->startRendering();
+
+            Ogre::WindowEventUtilities::removeWindowEventListener(pRenderWindow, spWindowEventListener.get());
+         }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Fatal exception.  Shutting down.\n"
+                 << "Exception: " << e.what() << "\n";
+    }
+    return 0;
+}
