31d497922543488c5e4b783b695620fdf64ee773
athile <athile.g@gmail.com>
2010-10-21 15:14:34 +0100

    Update.

--- lx0/dev/samples/v8/sm_v8_basic/main.cpp
+++ lx0/dev/samples/v8/sm_v8_basic/main.cpp
@@ -1,18 +1,48 @@
 // Adapted directly from: http://code.google.com/apis/v8/get_started.html
 
 #include <iostream>
+#include <functional>
+#include <string>
 #include <v8/v8.h>
 
 using namespace v8;
+
+void print (std::string s)
+{
+    std::cout << s;
+}
+
+Handle<Value> 
+func (const Arguments& args) 
+{
+    if (args.Length() != 1) 
+    {
+        return v8::Undefined();
+    }
+    else
+    {
+        HandleScope scope;
+        Handle<Value> arg = args[0];
+        String::AsciiValue value (arg);
+        print(*value);
+        return v8::Undefined();
+    }
+};
+
 
 int 
 main (int argc, char** argv)
 {
     HandleScope handle_scope;
-    Persistent<Context> context = Context::New();
+    
+    Handle<ObjectTemplate> global = ObjectTemplate::New();
+    global->Set(String::New("print"), FunctionTemplate::New(func));
+
+
+    Persistent<Context> context = Context::New(0, global);
   
     Context::Scope context_scope(context);
-    Handle<String> source = String::New("'Hello World!'");
+    Handle<String> source = String::New("print('Hello '); 'World!'");
     Handle<Script> script = Script::Compile(source);
 
     Handle<Value> result = script->Run();
