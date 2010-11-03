8c0497bdfcc7f263c6d7d1d6d910d3a78f86f186
athile <athile.g@gmail.com>
2010-11-03 14:30:52 +0000

    Add batch file for setting up VS command-line tools.

--- lx0/build/gen_vs10.bat
+++ lx0/build/gen_vs10.bat
@@ -1,12 +1,6 @@
 @echo off
 
-@rem vsvars32.bat does not check if it has already been run and can overflow
-@rem the PATH variable if it is run continually
-@rem ----
-@if "%GEN_VS10_RUN%" == "" (
-   call "%VS100COMNTOOLS%vsvars32.bat"
-   set "GEN_VS10_RUN=1"
-)
+call vs10env.bat
 
 echo.
 erase CMakeCache.txt
