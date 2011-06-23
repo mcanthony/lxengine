a79e526b1bf456247a710b7d5d64ebda26df1cb0
athile <athile.g@gmail.com>
2011-06-23 14:44:23 -0400

    File reorganization and add a logo image to the media directory

---
+++ lx0/dev/libs/lxengine/src/engine/view.cpp
@@ -0,0 +1,219 @@
+//===========================================================================//
+/*
+                                   LxEngine
+
+    LICENSE
+
+    Copyright (c) 2010-2011 athile@athile.net (http://www.athile.net)
+
+    Permission is hereby granted, free of charge, to any person obtaining a 
+    copy of this software and associated documentation files (the "Software"), 
+    to deal in the Software without restriction, including without limitation 
+    the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+    and/or sell copies of the Software, and to permit persons to whom the 
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+    IN THE SOFTWARE.
+*/
+//===========================================================================//
+
+//===========================================================================//
+//   H E A D E R S
+//===========================================================================//
+
+// Standard headers
+#include <cassert>
+#include <memory>
+
+// Public headers
+#include <lx0/engine/view.hpp>
+#include <lx0/lxengine.hpp>
+#include <lx0/engine/document.hpp>
+#include <lx0/engine/element.hpp>
+#include <lx0/engine/engine.hpp>
+
+// Internal headers
+#include "view_input.hpp"
+
+namespace lx0 { namespace engine { namespace dom_ns {
+
+
+    View::View (std::string impType, Document* pDocument)
+        : mpDocument (pDocument)
+        , mpDocForwarder (nullptr)
+    {
+        Engine::acquire()->incObjectCount("View");
+
+        lx_check_error(pDocument != nullptr, "Views must have a valid host Document");
+
+        // Create the view implementation object
+        //
+        EnginePtr spEngine = Engine::acquire();
+        static bool once = false;
+        if (!once) 
+        {
+            once = true;
+            spEngine->addViewPlugin("OGRE", [&](View* pView) { return _createViewImpOgre(pView); } );
+        }
+        ViewImp* pImp = spEngine->_createViewImp(impType, this);
+        mspImp.reset( pImp );
+
+        //
+        // Hook into Document events
+        //
+        mpDocForwarder = new DocForwarder(this);
+        pDocument->attachComponent("_docforwarder", mpDocForwarder);
+
+        mOnElementRemovedId = (mpDocument->slotElementRemoved += [&](ElementPtr spElem) { _onElementRemoved(spElem); }); 
+        mOnElementAddedId = (mpDocument->slotElementAdded += [&](ElementPtr spElem) { _onElementAdded(spElem); });
+    }
+
+    View::~View()
+    {
+        mpDocument->slotElementAdded -= mOnElementAddedId;
+        mpDocument->slotElementRemoved -= mOnElementRemovedId; 
+
+        mspImp->destroyWindow();
+        Engine::acquire()->decObjectCount("View");
+    }
+
+    
+    DocumentPtr 
+    View::document (void)  
+    { 
+        return mpDocument->shared_from_this(); 
+    }
+
+    void
+    View::_onElementAdded (ElementPtr spElem)
+    {
+        mspImp->_onElementAdded(spElem);
+    }
+
+    void
+    View::_onElementRemoved (ElementPtr spElem)
+    {
+        mspImp->_onElementRemoved(spElem);
+    }
+
+    /*!
+        Makes the view or window visible.
+     */
+    void 
+    View::show(lxvar options)
+    {
+        size_t hWindowHandle;
+        unsigned int width, height;
+        mspImp->createWindow(this, hWindowHandle, width, height, options);
+
+        // Create the input manager for the window, now that the window has been created
+        mspLxInputManager.reset( new detail::LxInputManager(hWindowHandle, width, height) );
+        mspLxInputManager->slotKeyDown += [&] (KeyEvent& e) { this->slotKeyDown(e); };
+    
+        mspImp->show(this, mpDocument);
+    }
+
+    void 
+    View::show (void)
+    {
+        show(lxvar());
+    }
+
+    void
+    View::updateBegin()
+    {
+        mspImp->updateBegin();
+    }
+
+    void
+    View::updateEnd()
+    {
+        mspImp->updateEnd();
+    }
+
+    void        
+    View::sendEvent (std::string evt, lxvar params)
+    {
+        mspImp->handleEvent(evt, params);
+
+        for (auto it = mControllers.begin(); it != mControllers.end(); ++it)
+            (*it)->handleEvent(evt, params);
+    }
+
+    /*
+        Called by OGRE between queuing up call the render calls and the GPU actually
+        blitting the frame: i.e. time when the CPU might potentially be idle waiting
+        for the GPU.
+     */
+    void
+    View::notifyViewImpIdle()
+    {
+        mspLxInputManager->update();
+    }
+
+    void
+    View::updateFrame()
+    {
+        mspImp->updateFrame(mpDocument->shared_from_this());
+    }
+
+    bool
+    View::isKeyDown (int keyCode) const
+    {
+        return mspLxInputManager->isKeyDown(keyCode);
+    }
+
+    void        
+    View::addController (Controller* pController)
+    {
+        mControllers.push_back( ControllerPtr(pController) );
+    }
+
+    //===========================================================================//
+
+    void View::DocForwarder::onAttached (DocumentPtr spDocument)
+    {
+        mpView->foreachComponent([&](ComponentPtr spComp) { 
+            spComp->onAttached(spDocument);
+        });
+    }
+
+    void View::DocForwarder::onUpdate (DocumentPtr spDocument)
+    {
+        mpView->foreachComponent([&](ComponentPtr spComp) { 
+            spComp->onUpdate(spDocument);
+        });
+    }
+
+    void View::DocForwarder::onElementAdded (DocumentPtr spDocument, ElementPtr spElem)
+    {
+        mpView->foreachComponent([&](ComponentPtr spComp) { 
+            spComp->onElementAdded(spDocument, spElem);
+        });
+    }
+
+    void View::DocForwarder::onElementRemoved (Document*   pDocument, ElementPtr spElem)
+    {
+        mpView->foreachComponent([&](ComponentPtr spComp) { 
+            spComp->onElementRemoved(pDocument, spElem);
+        });
+    }
+
+    //===========================================================================//
+
+    KeyboardState::KeyboardState()
+    {
+        for (int i = 0; i < KC_COUNT; ++i)
+            bDown[i] = false;
+    }
+
+}}}
---
+++ lx0/dev/libs/lxengine/src/engine/view_input.cpp
@@ -0,0 +1,206 @@
+//===========================================================================//
+/*
+                                   LxEngine
+
+    LICENSE
+
+    Copyright (c) 2010 athile@athile.net (http://www.athile.net)
+
+    Permission is hereby granted, free of charge, to any person obtaining a 
+    copy of this software and associated documentation files (the "Software"), 
+    to deal in the Software without restriction, including without limitation 
+    the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+    and/or sell copies of the Software, and to permit persons to whom the 
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+    IN THE SOFTWARE.
+*/
+//===========================================================================//
+
+//===========================================================================//
+//   H E A D E R S
+//===========================================================================//
+
+#include <iostream>
+#include <string>
+
+#include <lx0/lxengine.hpp>
+#include <lx0/util/misc/util.hpp>
+#include <lx0/engine/engine.hpp>
+#include <lx0/engine/document.hpp>
+#include "view_input.hpp"
+
+#include <OIS/OISMouse.h>
+#include <OIS/OISKeyboard.h>
+#include <OIS/OISJoyStick.h>
+#include <OIS/OISInputManager.h>
+
+using namespace OIS;
+using namespace lx0::util;
+
+//===========================================================================//
+//   I M P L E M E N T A T I O N 
+//===========================================================================//
+
+namespace lx0 { namespace engine { namespace dom_ns { namespace detail {
+
+    using OIS::KeyEvent;
+
+    /*
+        Developer notes:
+
+        In general, multiple inheritance is avoided as can unnecessarily complicate
+        the code.  For example, the View class does not inherit from OGRE's
+        FrameListener or WindowListener classes as that would expose OGRE in the
+        public header for the View (the goal is to keep the View as independent from
+        the implementation as possible so other implementations can be supported
+        in the future).  In this particular case, InputImp is a small leaf class 
+        that is internal only, so for simplicity, just go ahead and use multiple 
+        inhertance.
+     */
+    class InputImp
+        : public OIS::KeyListener
+        , public OIS::MouseListener
+    {
+    public:
+        ///@todo pass in the window handle rather than an OGRE pointer - no need for OGRE here
+        InputImp (LxInputManager* pInterface, size_t hWindowHandle, unsigned int windowWidth, unsigned int windowHeight)
+            : mpInterface (pInterface)
+        {
+            ParamList params;
+            params.insert( std::make_pair(std::string("WINDOW"), lx_itoa(hWindowHandle)) );
+            
+            // Don't hide the cursor
+            params.insert( std::make_pair("w32_mouse", "DISCL_FOREGROUND") );
+            params.insert( std::make_pair("w32_mouse", "DISCL_NONEXCLUSIVE") );    
+
+            mpInputManager = InputManager::createInputSystem(params);
+
+
+            // Adapted from the Irricht Engine:
+            // http://www.irrlicht3d.org/wiki/index.php?n=Main.IntegratingOISWithIrrlicht
+            //
+            {
+                const unsigned int v = mpInputManager->getVersionNumber();
+
+                lx_debug("OIS Information");
+                lx_debug("    Version: %d.%d.%d", (v>>16 ),((v>>8) & 0x000000FF),(v & 0x000000FF));
+                lx_debug("    Release Name: %s", mpInputManager->getVersionName().c_str());
+                lx_debug("    Manager: %s", mpInputManager->inputSystemName().c_str());
+                lx_debug("    Devices Keyboards: %d", mpInputManager->getNumberOfDevices(OISKeyboard));
+                lx_debug("    Devices Mice: %d", mpInputManager->getNumberOfDevices(OISMouse));
+                lx_debug("    Devices Joysticks: %d", mpInputManager->getNumberOfDevices(OISJoyStick));
+            }
+
+            // Create buffered devices
+            mpKeyboard = static_cast<OIS::Keyboard*>(mpInputManager->createInputObject( OISKeyboard, true ));
+            mpMouse = static_cast<OIS::Mouse*>(mpInputManager->createInputObject( OISMouse, true ));
+
+            // Set the window size on the mouse
+            mpMouse->getMouseState().width = windowWidth;
+            mpMouse->getMouseState().height = windowHeight;
+
+            // Set up the event listeners
+            mpKeyboard->setEventCallback(this);
+            mpMouse->setEventCallback(this);
+        }
+
+
+        ~InputImp()
+        {
+            lx_check_error( mpInputManager != nullptr );
+            lx_check_error( mpKeyboard != nullptr );
+            lx_check_error( mpMouse != nullptr );
+
+            mpKeyboard->setEventCallback(nullptr);
+            mpMouse->setEventCallback(nullptr);
+
+            mpInputManager->destroyInputObject( mpKeyboard );
+            mpInputManager->destroyInputObject( mpMouse );
+
+            InputManager::destroyInputSystem(mpInputManager);
+        }
+
+        void update()
+        {
+            mpKeyboard->capture();
+
+            // Test code...
+            if (mpKeyboard->isKeyDown(OIS::KC_ESCAPE))
+                Engine::acquire()->sendMessage("quit");
+        }
+
+        bool 
+        isKeyDown (int keyCode) const
+        {
+            return mpKeyboard->isKeyDown(OIS::KeyCode(keyCode));
+        }
+
+        virtual bool keyPressed( const KeyEvent &arg )
+        {
+            lx0::KeyEvent e;
+            e.keyCode = int(arg.key);
+            e.keyChar = char(arg.text);
+
+            mpInterface->slotKeyDown(e);
+
+            // true means to continue sending the event to any other listeners 
+            return true;
+        }
+
+		virtual bool keyReleased( const KeyEvent &arg )
+        {
+            return true;
+        }
+
+        virtual bool mouseMoved( const MouseEvent &arg )
+        {
+            return true;
+        }
+
+		virtual bool mousePressed( const MouseEvent &arg, MouseButtonID id )
+        {
+            return true;
+        }
+
+		virtual bool mouseReleased( const MouseEvent &arg, MouseButtonID id )
+        {
+            return true;
+        }
+
+    protected:
+        LxInputManager*         mpInterface;
+        OIS::InputManager*      mpInputManager;
+        OIS::Keyboard*          mpKeyboard;
+        OIS::Mouse*             mpMouse;
+    };
+
+
+
+    LxInputManager::LxInputManager (size_t hWindowHandle, unsigned int width, unsigned int height)
+        : mspImp ( new InputImp (this, hWindowHandle, width, height) )
+    {
+    }
+
+    void 
+    LxInputManager::update() 
+    { 
+        mspImp->update(); 
+    }
+
+    bool 
+    LxInputManager::isKeyDown (int keyCode) const
+    {
+        return mspImp->isKeyDown(keyCode);
+    }
+
+}}}}
