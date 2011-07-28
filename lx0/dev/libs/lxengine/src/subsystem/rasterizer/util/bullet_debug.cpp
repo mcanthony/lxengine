adcd5c4311c44e13e7c41d50134561c846f820dc
athile <athile.g@gmail.com>
2011-07-28 14:20:44 -0400

    Clean-up only

--- lx0/dev/libs/lxengine/src/subsystem/rasterizer/util/bullet_debug.cpp
+++ lx0/dev/libs/lxengine/src/subsystem/rasterizer/util/bullet_debug.cpp
@@ -17,38 +17,31 @@ extern PFNGLUSEPROGRAMPROC gl3wUseProgram;
 #include <lx0/lxengine.hpp>
 #include <lx0/subsystem/physics.hpp>
 
-
-class GLDebugDrawer : public btIDebugDraw
+class DebugDraw : public btIDebugDraw
 {
-        int m_debugMode;
-      
-   public:
-      
-        GLDebugDrawer();
+public:
+                    DebugDraw();
             
-        virtual void   drawLine(const btVector3& from,const btVector3& to,const btVector3& color);
-      
-        virtual void   drawContactPoint(const btVector3& PointOnB,const btVector3& normalOnB,btScalar distance,int lifeTime,const btVector3& color);
-      
-        virtual void   reportErrorWarning(const char* warningString);
-      
-        virtual void   draw3dText(const btVector3& location,const char* textString);
-      
-        virtual void   setDebugMode(int debugMode);
-      
-        virtual int      getDebugMode() const { return m_debugMode;}
-      
+    virtual void    drawLine            (const btVector3& from,const btVector3& to,const btVector3& color);
+    virtual void    drawContactPoint    (const btVector3& PointOnB,const btVector3& normalOnB,btScalar distance,int lifeTime,const btVector3& color);
+    virtual void    reportErrorWarning  (const char* warningString);
+    virtual void    draw3dText          (const btVector3& location,const char* textString);
+    virtual void    setDebugMode        (int debugMode);
+    virtual int     getDebugMode        (void) const { return m_debugMode;}
+
+protected:
+         int m_debugMode;     
 };
 
-GLDebugDrawer::GLDebugDrawer()
-:m_debugMode(btIDebugDraw::DBG_DrawWireframe)
+DebugDraw::DebugDraw()
+    : m_debugMode   (btIDebugDraw::DBG_DrawWireframe)
 {
    
 }
 
-void    GLDebugDrawer::drawLine(const btVector3& from,const btVector3& to,const btVector3& color)
+void    
+DebugDraw::drawLine(const btVector3& from,const btVector3& to,const btVector3& color)
 {
-    glUseProgram(0);
     glBegin(GL_LINES);
     glColor3f(color.getX(), color.getY(), color.getZ());
     glVertex3f(from.getX(), from.getY(), from.getZ());
@@ -56,36 +49,25 @@ void    GLDebugDrawer::drawLine(const btVector3& from,const btVector3& to,const 
     glEnd();
 }
 
-void    GLDebugDrawer::setDebugMode(int debugMode)
+void    
+DebugDraw::setDebugMode(int debugMode)
 {
    m_debugMode = debugMode;
 }
 
-void    GLDebugDrawer::draw3dText(const btVector3& location,const char* textString)
+void    
+DebugDraw::draw3dText(const btVector3& location,const char* textString)
 {
-   //glRasterPos3f(location.x(),  location.y(),  location.z());
-   //BMF_DrawString(BMF_GetFont(BMF_kHelvetica10),textString);
 }
 
-void    GLDebugDrawer::reportErrorWarning(const char* warningString)
+void    
+DebugDraw::reportErrorWarning (const char* warningString)
 {
-   printf(warningString);
 }
 
-void    GLDebugDrawer::drawContactPoint(const btVector3& pointOnB,const btVector3& normalOnB,btScalar distance,int lifeTime,const btVector3& color)
+void    
+DebugDraw::drawContactPoint(const btVector3& pointOnB,const btVector3& normalOnB,btScalar distance,int lifeTime,const btVector3& color)
 {
-   {
-      //btVector3 to=pointOnB+normalOnB*distance;
-      //const btVector3&from = pointOnB;
-      //glColor4f(color.getX(), color.getY(), color.getZ(), 1.0f);   
-      
-      //GLDebugDrawer::drawLine(from, to, color);
-      
-      //glRasterPos3f(from.x(),  from.y(),  from.z());
-      //char buf[12];
-      //sprintf(buf," %d",lifeTime);
-      //BMF_DrawString(BMF_GetFont(BMF_kHelvetica10),buf);
-   }
 }
 
 
@@ -104,7 +86,7 @@ void enable_bullet_debug()
         if (spPhysicsDoc)
         {
             auto pWorld = spPhysicsDoc->getWorld();
-            pWorld->setDebugDrawer(new GLDebugDrawer);
+            pWorld->setDebugDrawer(new DebugDraw);
         }
     }
 }
@@ -119,6 +101,7 @@ void draw_bullet_debug(const glm::mat4& projMatrix, const glm::mat4& viewMatrix)
         auto spPhysicsDoc = (*it)->getComponent<lx0::IPhysicsDoc>();
         if (spPhysicsDoc)
         {
+            glUseProgram(0);
             glDepthRange (0.0f, 0.999999f);
 
             glMatrixMode(GL_PROJECTION);
