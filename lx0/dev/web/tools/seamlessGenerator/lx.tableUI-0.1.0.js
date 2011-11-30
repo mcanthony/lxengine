85eb9cdd375ce74bf010a3ac2c22ea47b3942086
athile <athile.g@gmail.com>
2011-11-30 11:32:15 -0700

    Changed the seamless image generator to use a task queue for better page responsiveness

---
+++ lx0/dev/web/tools/seamlessGenerator/lx.tableUI-0.1.0.js
@@ -0,0 +1,96 @@
+//
+// Table-UI: a lightweight, functional automatic UI generator for rapid prototyping
+//
+var tableUI =
+{
+    _generators : 
+    {
+        _label : function(data)
+        {
+            var td = $("<td/>");
+            var span = $("<span/>");
+            span.css("padding-right", "1em");
+            span.text(data.label);
+            td.append(span);
+            return td;
+        },
+
+        seed : function (ui, options, data)
+        {
+            var value;
+
+            var tr = $("<tr/>");
+            tr.append( this._label(data) );
+                        
+            {
+                var td = $("<td/>");
+                value = $("<input/>");
+                value.attr("type", "input");
+                value.val(options[data.label]);
+
+                var button = $("<input/>");
+                button.attr("type", "button");
+                button.val("random");
+                button.click(function() {
+                    value.val( Math.floor(Math.random() * 10000) ).trigger('change');
+                });
+
+                td.append(value);
+                td.append(button);
+                tr.append(td);
+
+                value.change(function () {                    var value = $(this).val();                    options[data.label] = value;                    ui.onchange(value);                });            }
+            return tr;
+        },
+
+        "int" : function (ui, options, data)
+        {
+            var tr = $("<tr/>");
+            tr.append( this._label(data) );
+
+            {
+                var td = $("<td/>");
+                value = $("<input/>");
+                value.attr("type", "input");
+                value.val(options[data.label]);
+                            
+                td.append(value);
+                tr.append(td);
+
+                value.change(function () {                    var value = parseInt($(this).val());                    options[data.label] = value;                    ui.onchange(value);                });            }
+            return tr;
+        },
+
+        "float" : function (ui, options, data)
+        {
+            var tr = $("<tr/>");
+            tr.append( this._label(data) );
+
+            {
+                var td = $("<td/>");
+                value = $("<input/>");
+                value.attr("type", "input");
+                value.val(options[data.label]);
+                            
+                td.append(value);
+                tr.append(td);
+
+                value.change(function () {                    var value = parseFloat($(this).val());                    options[data.label] = value;                    ui.onchange(value);                });            }
+            return tr;
+        }
+    },
+
+    generate : function(anchor, options, elements, params)
+    {
+        var ui = 
+        {
+            onchange : params.onchange,
+        };
+
+        var _this = this;
+        $.each(elements, function(i,data) {
+            var tr = _this._generators[data.type](ui, options, data);
+            $(anchor).append(tr);
+        });
+    },
+};
---
+++ lx0/dev/web/tools/shared/script/lx.core-0.1.0.js
@@ -0,0 +1,78 @@
+
+try { eval("lx"); } catch (e) { lx = {}; }
+if (!lx.core) lx.core = {};
+
+(function (NS) {
+   
+    NS.clamp = function(i, n, m) {
+        return Math.max(Math.min(i, m), n);
+    };
+    NS.random = function (min, max) {
+        return Math.random() * (max - min) + min;
+    };
+
+    NS.trim = function(s) {
+        return s.replace(/^\s+|\s+$/g,"")    
+    };
+
+
+    /*
+        Takes an array of function objects and runs them with the
+        specified delay between each call. 
+     */
+    NS.run_tasks = function(tasks, delay, params) {
+        delay = delay || 0;
+        if (!params)
+        {
+            function worker(tasks) {
+                if (tasks.length > 0) {
+                    var task = tasks.shift();
+                    window.setTimeout(function () {
+                        task();
+                        worker(tasks);
+                    }, delay);
+                }
+            }
+            worker(tasks);
+        }
+        else
+        {
+            var count = 0;
+            var initialLength = tasks.length;
+            var groupSize = params.groupSize || 5;
+
+            function worker(tasks) {
+                if (tasks.length > 0) {
+
+                    var group = [];
+                    for (var i = 0; i < Math.min(groupSize, tasks.length); ++i)
+                        group.push( tasks.shift() );
+
+                    window.setTimeout(function () {
+                        
+                        for (var i = 0; i < group.length; ++i)
+                            group[i]();
+                        count += group.length;
+                        
+                        if (params.callback)
+                            params.callback(count);
+
+                        worker(tasks);
+                    }, delay);
+                }
+                else {
+                    if (count == initialLength) 
+                        if (params.oncomplete)
+                            params.oncomplete();
+                    else
+                        if (params.oncancel)
+                            params.oncancel(); 
+                }
+            }
+            worker(tasks);
+        }
+        return tasks;
+    };
+
+})(lx.core);
+
