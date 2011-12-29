8f62d57ce64976215b7da5735039bc61714379f7
athile <athile@athile.net>
2011-12-28 19:03:07 -0700

    Add utility function for automatically displaying source code

---
+++ lx0/dev/web/site/tools/shared/script/lx.webUtil-0.1.0.js
@@ -0,0 +1,59 @@
+(function ($) {
+
+    var methods =
+    {
+        build: function () {
+            var anchorNames = [];
+            $("h2").each(function (i) {
+                var anchor = $("<a/>");
+                var name = "toc_" + i;
+                anchor.attr("name", name);
+                anchorNames.push([name, $(this).text()]);
+                $(this).before(anchor);
+            });
+
+            var list = $("<ol/>");
+            $.each(anchorNames, function (i, value) {
+                var item = $("<li/>");
+                var link = $("<a/>");
+                link.text(value[1]);
+                link.attr("href", "#" + value[0]);
+                item.html(link);
+                list.append(item);
+            });
+            $(this).html(list);
+        },
+    };
+
+    $.fn.tableOfContents = function () {
+
+        return this.each(function () {
+            methods['build'].call(this);
+        });
+    };
+
+})(jQuery);
+
+(function ($) {
+
+    $.fn.includeSource = function () {
+        return this.each(function() {
+
+            var elem = this;
+            var url = $(this).attr("data-source");
+            $.ajax(url + "?rand=" + Math.random(), {
+                dataType : "text",
+                async : true,
+                success : function(text) {
+                    $(elem).text(text);
+                    $(elem).after("<p><em>Source highlighting provided by <a href='http://softwaremaniacs.org/soft/highlight/en/'>highlight.js</a></em></p>");
+                    $(elem).wrap("<code/>").wrap("<pre class='codeview language-javascript'/>").each(function(i, e) { 
+                        hljs.highlightBlock(e, '    ', false); 
+                    });
+                },
+            });
+
+        });
+    };
+
+})(jQuery);
