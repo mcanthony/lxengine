938d01c6298e7361e6d498678f34a87eba319bac
athile <athile@athile.net>
2012-01-07 10:30:00 -0700

    Add statement, statementSet, and functionDefinition parsing to LxLang3

--- lx0/dev/web/site/tools/lxlang/lx.lxlang3.js
+++ lx0/dev/web/site/tools/lxlang/lx.lxlang3.js
@@ -3,132 +3,146 @@ lx.lxlang3 = {};
 
 (function(NS) {
     
-    NS.Lexer = (function() {
 
-        function _parseTokens(text)
+    function _error (s)
+    {
+        for (var i = 1; i < arguments.length; ++i)
         {
-            var Terminals  =
-            {
-                "whitespace" : 
-                { 
-                    re : /^[\s\n]+/, 
-                    discard : true 
-                },
-                "name" : 
-                { 
-                    re : /^[A-Za-z\$_][\$A-Za-z0-9_]*/ 
-                }, 
-                "number" : 
-                {
-                    reArr : [
-                        /^[0-9]+(\.[0-9]+)?/,
-                        /^\.[0-9]+/
-                    ],
-                },
-                "symbol" : 
-                {
-                    arr : [ 
-                        "+", "-", "*", "/",
-                        "%",
-                        "=",
-                        "==", "<", ">", "<=", ">=",
-                        "(", ")",
-                        "{", "}",
-                        "[", "]",
-                        ";", ",",
-                        "?", ":",
-                        "->", "."
-                    ],
-                },
-                "comment" :
-                {
-                    re : /^\/\/[^\n]*\n/,
-                    discard : true,
-                },
-                "unrecognized" :
-                {
-                },
-            };
+            var regex = new RegExp("%" + i + "%", "g");
+            s = s.replace(regex, arguments[i]);
+        }
+        console.log(s);
+        throw s;
+    }
 
-            var tokens = [];
-            
-            while (text.length)
+    /*
+        Convert the text string into a token array.
+     */
+    NS.tokenize = function (text)
+    {
+        var Terminals  =
+        {
+            "whitespace" : 
+            { 
+                re : /^[\s\n]+/, 
+                discard : true 
+            },
+            "name" : 
+            { 
+                re : /^[A-Za-z\$_][\$A-Za-z0-9_]*/ 
+            }, 
+            "number" : 
             {
-                //
-                // Convenience function to set a new match if it is longer than the
-                // current match or there has not yet been a match
-                // 
-                var match = undefined;
-                function setMatch (type, terminal, text)
+                reArr : [
+                    /^[0-9]+(\.[0-9]+)?/,
+                    /^\.[0-9]+/
+                ],
+            },
+            "symbol" : 
+            {
+                arr : [ 
+                    "+", "-", "*", "/",
+                    "%",
+                    "=",
+                    "==", "<", ">", "<=", ">=",
+                    "(", ")",
+                    "{", "}",
+                    "[", "]",
+                    ";", ",",
+                    "?", ":",
+                    "->", "."
+                ],
+            },
+            "comment" :
+            {
+                re : /^\/\/[^\n]*\n/,
+                discard : true,
+            },
+            "unrecognized" :
+            {
+            },
+        };
+
+        var tokens = [];
+            
+        while (text.length)
+        {
+            //
+            // Convenience function to set a new match if it is longer than the
+            // current match or there has not yet been a match
+            // 
+            var match = undefined;
+            function setMatch (type, terminal, text)
+            {
+                if (!match || text.length > match.text.length)
                 {
-                    if (!match || text.length > match.text.length)
-                    {
-                        match = {
-                            type : type,
-                            terminal : terminal,
-                            text : text
-                        };
+                    match = {
+                        type : type,
+                        terminal : terminal,
+                        text : text
+                    };
+                }
+            }
+
+            //
+            // Loop through all the tests in the terminal table
+            //
+            for (var key in Terminals) {
+                var terminal = Terminals[key];
+
+                if (terminal.re)
+                {
+                    var m;
+                    if (m = terminal.re.exec(text)) {
+                        setMatch(key, terminal, m[0]);
                     }
                 }
-
-                //
-                // Loop through all the tests in the terminal table
-                //
-                for (var key in Terminals) {
-                    var terminal = Terminals[key];
-
-                    if (terminal.re)
+                if (terminal.reArr)
+                {
+                    for (var i = 0; i < terminal.reArr.length; ++i)
                     {
-                        var m;
-                        if (m = terminal.re.exec(text)) {
+                        if (m = terminal.reArr[i].exec(text)) {
                             setMatch(key, terminal, m[0]);
                         }
                     }
-                    if (terminal.reArr)
-                    {
-                        for (var i = 0; i < terminal.reArr.length; ++i)
-                        {
-                            if (m = terminal.reArr[i].exec(text)) {
-                                setMatch(key, terminal, m[0]);
-                            }
-                        }
-                    }
-                    if (terminal.arr)
-                    {
-                        for (var i = 0; i < terminal.arr.length; ++i)
-                        {
-                            var s = terminal.arr[i];
-                            if (text.substr(0, s.length) == s)
-                                setMatch(key, terminal, s);
-                        }
-                    }
                 }
-
-                //
-                // Set a single character match if no rules were matched
-                //
-                setMatch("unrecognized", Terminals["unrecognized"], text.substr(0,1));
-
-                //
-                // Advanced the text stream and push the token into the stream
-                //
-                text = text.substr(match.text.length);
-                if (!match.terminal.discard)
+                if (terminal.arr)
                 {
-                    tokens.push({ type : match.type, value : match.text });
+                    for (var i = 0; i < terminal.arr.length; ++i)
+                    {
+                        var s = terminal.arr[i];
+                        if (text.substr(0, s.length) == s)
+                            setMatch(key, terminal, s);
+                    }
                 }
             }
-            return tokens;
-        }
 
-        var ctor = function(arg)
+            //
+            // Set a single character match if no rules were matched
+            //
+            setMatch("unrecognized", Terminals["unrecognized"], text.substr(0,1));
+
+            //
+            // Advanced the text stream and push the token into the stream
+            //
+            text = text.substr(match.text.length);
+            if (!match.terminal.discard)
+            {
+                tokens.push({ type : match.type, value : match.text });
+            }
+        }
+        return tokens;
+    };
+
+    /*
+        Conditionally advance along a token array based on basic pattern matching.
+     */
+    NS.Lexer = (function() {
+
+        var ctor = function(tokens)
         {
             this._index = 0;
-
-            if (typeof arg == "string")
-                this._tokens = _parseTokens(arg);
-            else
-                this._tokens = arg;
+            this._tokens = tokens;
         };
 
         var methods = 
@@ -158,28 +172,39 @@ lx.lxlang3 = {};
                 }
                 else
                 {
-                    var pattern = arguments[0];
+                    var match = [];
 
-                    var match;
-                    if (pattern.substr(0,1) == "@") {
-                        if (next.type == pattern.substr(1))
-                            match = [ next ];
+                    for (var i = 0; i < arguments.length; ++i)
+                    {
+                        var pattern = arguments[i];
+                        var next = this.peek(i);
+
+                        if (pattern.substr(0,1) == "@") {
+                            if (next.type == pattern.substr(1))
+                                match.push(next);
+                        }
+                        else {
+                            if (next.value == pattern)
+                                match.push(next);
+                        }
                     }
-                    else {
-                        if (next.value == pattern)
-                            match = [ next ];
-                    }
+
+                    // At least one requested match failed, so fail completely
+                    if (match.length != arguments.length)
+                        match = undefined;
                 }
 
                 if (match)
-                    this._index++;
+                    this._index += match.length;
 
                 return match;    
             },
 
-            peek : function() 
+            peek : function(offset) 
             {
-                return this._tokens[this._index];
+                offset = offset || 0;
+                var next = this._tokens[this._index + offset];
+                return next ? next : {};
             },
         };
 
@@ -189,7 +214,7 @@ lx.lxlang3 = {};
         return ctor;
     })();
 
-    NS.Parser = (function() {
+    NS.ParserStage1 = (function() {
 
         var ctor = function(lex)
         {
@@ -221,22 +246,72 @@ lx.lxlang3 = {};
                 return value;
             },
 
-            'function' : function (lex)
-            {
-            },
-
             'statement' : function (lex)
             {
-                var statements = [];
+                if (lex.empty()) _error("E1016 Cannot parse empty statement");
 
                 var tokens = [];
-                while (lex.peek() && lex.peek().value != ';')
+                while (!lex.empty() && lex.peek().value != ';')
                 {
                     tokens.push(lex.consume()[0]);
                 }
-                lex.consume(';') || this.error("EP1002", "Did not find expected ;");
+                lex.consume(';') || _error("E1002: Did not find expected ;");
                 
                 return { type : 'statement', tokens : tokens };
+            },
+
+            'statementSet' : function (lex)
+            {
+                if (lex.empty()) _error("E1016 Cannot parse empty statementSet");
+                
+                var statements = [];
+
+                while (!lex.empty() && lex.peek().value != '}')
+                {
+                    var statement = this.parse('statement') || _error("Could not parse Statement");
+                    statements.push(statement);
+                }
+                return { type : 'statementSet', statements : statements };
+            },
+
+            'variableDeclaration' : function(lex)
+            {
+                var dec = lex.consume("@name", "@name") || _error("Expected variable declaration");
+                return { type : "variableDeclaration", type : dec[0].value, name : dec[1].value };
+            },
+
+            'argumentList' : function (lex)
+            {
+                var ast = { type : "argumentList", list : [] };
+
+                var dec = this.parse('variableDeclaration');
+                while (lex.consume(","))
+                {
+                    ast.list.push(dec);
+                    dec = this.parse('variableDeclaration');
+                }
+                ast.list.push(dec);
+                
+                return ast;
+            },
+
+            'function' : function (lex)
+            {
+                var ast = { type : 'function' };
+                
+                var header1 = lex.consume("function", "@name", "(");
+                ast.argumentList = this.parse('argumentList');
+                var header2 = lex.consume(")", "->", "@name");
+                
+                ast.name = header1[1].value;
+                ast.returnType = header2[2].value;
+
+                lex.consume("{");
+                while (lex.peek().value != "}")
+                {
+                    ast.body = this.parse('statementSet');
+                }
+                return ast;
             },
 
             /*
@@ -327,11 +402,12 @@ lx.lxlang3 = {};
             {
                 var startPos = this._lex.tell();
 
-                var ast = parsers[type].call(this, this._lex, options);
+                var parseFunction = parsers[type] || _error("No parse function for %1%", JSON.stringify(type));
+                var ast = parseFunction.call(this, this._lex, options);
                 var endPos = this._lex.tell();
                     
                 return ast;
-            }
+            },
         };
 
         for (var key in methods)
@@ -339,5 +415,112 @@ lx.lxlang3 = {};
 
         return ctor;
     })();
+
+    NS.ParserStage2 = (function() {
+        
+        var transforms =
+        {
+            'statement' : function(ast)
+            {
+                var lex = new NS.Lexer(ast.tokens);
+                var parser = new NS.ParserStage1(lex);
+                
+                var next = lex.peek();
+                
+                if (next.value == "var")
+                {
+                    lex.consume("var");
+                    var name = lex.consume("@name")[0].value;
+                    lex.consume("=");
+                    var expr = parser.parse('expression');
+                    lex.consume(";");
+                    return { type : 'assignment', variable: name, value : expr };                    
+                }   
+                else
+                {
+                    var expr = parser.parse('expression');
+                    lex.consume(";");
+                    return { type : 'statement', value : expr };
+                }
+            },
+
+            'statementSet' : function (ast)
+            {
+                for (var i = 0; i < ast.statements.length; ++i)
+                    ast.statements[i] = this.parse(ast.statements[i]);
+                return ast;
+            },
+
+            'function' : function (ast)
+            {
+                ast.body = this.parse(ast.body);
+                return ast;
+            }
+        };
+
+        var methods =
+        {
+            parse : function (ast, options)
+            {
+                var type = ast.type;
+                var transform = transforms[type];
+                return (transform) ? transform.call(this, ast, options) : ast;
+            }
+        };
+        
+        var ctor = function() 
+        {
+        };
+
+        for (var name in methods)
+            ctor.prototype[name] = methods[name];
+        return ctor;
+    })();
+
+    NS.Parser2 = (function() {
+
+        var methods = 
+        {
+            _parseStage1 : function(lexer, nonterminal)
+            {
+                var parser = new NS.ParserStage1(lexer);
+                return parser.parse(nonterminal);
+            },
+
+            _parseStage2 : function(ast)
+            {
+                var parser = new NS.ParserStage2();
+                return parser.parse(ast);
+            },
+
+            parse : function(text, nonterminal)
+            {
+                var tokens = NS.tokenize(text);
+                var lexer = new NS.Lexer(tokens);
+                
+                var ast1 = this._parseStage1(lexer, nonterminal);
+                var ast2 = this._parseStage2(ast1);
+
+                return ast2;
+            }
+        };
+        
+        var ctor = function()
+        {
+        };
+        
+        for (var name in methods)
+            ctor.prototype[name] = methods[name];
+
+        return ctor;
+    })();
+
+    NS.parse = function (text, nonterminal)
+    {
+        nonterminal = nonterminal || "translationUnit";
+        
+        var parser = new NS.Parser2();
+        return parser.parse(text, nonterminal);
+    }
 
 })(lx.lxlang3);
